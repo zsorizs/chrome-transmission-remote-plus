862777fd99e26ad3a9f74e8f69ae2d84e619a886
Aleka <a@cherepanov.info>
2011-11-20 18:37:00 +0400

    popup refactored

--- source/js/class.js
+++ source/js/class.js
@@ -2,17 +2,27 @@
 
 function Torrent() {
 
-	const	TORRENT_WAIT_VERIFY	= 1
+	const	TORRENT_ERROR		= -1
+	,		TORRENT_WAIT_VERIFY	= 1
 	,		TORRENT_VERIFING	= 2
 	,		TORRENT_DOWNLOADING	= 4
 	,		TORRENT_SEEDING		= 8
 	,		TORRENT_PAUSED		= 16
 	;
 
+	var oPauseBtn
+	,	oResumeBtn
+	,	oRemoveBtn
+	,	oProgress
+	, 	oProgressBar
+	,	oStats
+	,	oSpeeds
+	,	oRoot = $('<li></li>')
+	;
+
 	this.id = 0;
 	this.name = '';
 	this.status = 0;
-	this.elem = document.createElement('li');
 
 	// send RPC for a torrent
 	this.sendRPC = function(method, ctrlDown) {
@@ -34,220 +44,138 @@ function Torrent() {
 		;
 
 		if ((type == 0 || this.status == type) && this.name.search(filter) > -1) {
-			$('#list').append(this.elem);
+			$('#list').append(oRoot);
 		} else {
-			$('#list_hidden').append(this.elem);
+			$('#list_hidden').append(oRoot);
 		}
 	};
 
-	// create the list element and update torrent properties
-	this.createElem = function(props) {
-		if (!!props.id) this.id = props.id;
-		if (!!props.name) this.name = props.name;
-		if (!!props.status) this.status = props.status;
+	function adjustButtons(status) {
+		oPauseBtn.toggle(status === TORRENT_DOWNLOADING || status === TORRENT_SEEDING);
+		oResumeBtn.toggle(status === TORRENT_PAUSED);
+	}
 
-		var thisTorrent = this,
-			percentDone = 100 - (props.leftUntilDone / props.sizeWhenDone * 100),
-			nameElem = document.createElement('div'),
-			statsElem = document.createElement('div'),
-			speedsElem = document.createElement('div'),
-			progressElem = document.createElement('div'),
-			curProgressElem = document.createElement('div'),
-			pauseElem = document.createElement('div'),
-			resumeElem = document.createElement('div'),
-			removeElem = document.createElement('div');
-
-		this.elem.appendChild(nameElem);
-		this.elem.appendChild(statsElem);
-		this.elem.appendChild(progressElem);
-		this.elem.appendChild(pauseElem);
-		this.elem.appendChild(resumeElem);
-		this.elem.appendChild(removeElem);
-
-		nameElem.className = 'torrent_name';
-		nameElem.setAttribute('title', props.name + '\n\nDownloaded to: ' + props.downloadDir);
-		nameElem.innerHTML = props.name;
-
-		statsElem.appendChild(speedsElem);
-		statsElem.className = 'torrent_stats';
-
-		speedsElem.className = 'torrent_speeds';
-
-		progressElem.appendChild(curProgressElem);
-		progressElem.className = 'torrent_progress';
-
-		if (percentDone === 100) {
-			curProgressElem.className = 'complete';
-			$(curProgressElem).css('width', percentDone + '%');
-		} else if (percentDone > 0) {
-			curProgressElem.className = 'downloading';
-			$(curProgressElem).css('width', percentDone + '%');
-		} else {
-			$(curProgressElem).hide();
-		}
-
-		pauseElem.className = 'torrent_button pause';
-		pauseElem.setAttribute('title', 'Pause');
-		pauseElem.addEventListener('click', function() { thisTorrent.sendRPC('torrent-stop'); }, true);
-
-		resumeElem.className = 'torrent_button resume';
-		resumeElem.setAttribute('title', 'Resume');
-		resumeElem.addEventListener('click', function() { thisTorrent.sendRPC('torrent-start'); }, true);
-
-		removeElem.setAttribute('name', 'torrent_remove');
-		removeElem.className = 'torrent_button remove';
-		removeElem.setAttribute('title', 'Double-click to remove torrent\n\nHold down CTRL to also delete data');
-		removeElem.addEventListener('dblclick', function() { thisTorrent.sendRPC('torrent-remove', event.ctrlKey); }, true);
-
-		$(pauseElem).toggle(props.status === TORRENT_DOWNLOADING || props.status === TORRENT_SEEDING);
-		$(resumeElem).toggle(props.status === TORRENT_PAUSED);
-
+	function adjustLabels(props) {
 		switch(props.status) {
 			case TORRENT_WAIT_VERIFY:
-				statsElem.appendChild(document.createTextNode(
-					formatBytes(props.sizeWhenDone - props.leftUntilDone) + ' of ' + formatBytes(props.sizeWhenDone) +
-					' (' + percentDone.toFixed(2) + '%) - waiting to verify local data'
-				));
-				speedsElem.innerHTML = '';
+				oStats.text(formatBytes(props.sizeWhenDone - props.leftUntilDone) + ' of ' + formatBytes(props.sizeWhenDone)
+							+ ' (' + percentDone.toFixed(2) + '%) - waiting to verify local data');
+				oSpeeds.text('');
 			break;
 			case TORRENT_VERIFING:
-				statsElem.appendChild(document.createTextNode(
-					formatBytes(props.sizeWhenDone - props.leftUntilDone) + ' of ' + formatBytes(props.sizeWhenDone) +
-					' (' + percentDone.toFixed(2) + '%) - verifying local data (' + (props.recheckProgress * 100).toFixed() + '%)'
-				));
-				speedsElem.innerHTML = '';
+				oStats.text(formatBytes(props.sizeWhenDone - props.leftUntilDone) + ' of ' + formatBytes(props.sizeWhenDone)
+							+ ' (' + percentDone.toFixed(2) + '%) - verifying local data (' + (props.recheckProgress * 100).toFixed() + '%)');
+				oSpeeds.text('');
 			break;
 			case TORRENT_DOWNLOADING:
 				if (props.metadataPercentComplete < 1) {
-					statsElem.appendChild(document.createTextNode('\
-						Magnetized transfer - retrieving metadata (' + (props.metadataPercentComplete * 100).toFixed() + '%)'
-					));
-					speedsElem.innerHTML = '';
-					progressElem.className = 'torrent_progress magnetizing';
+					oStats.text('Magnetized transfer - retrieving metadata (' + (props.metadataPercentComplete * 100).toFixed() + '%)');
+					oSpeeds.text('');
+					oProgressElem.attr('class', 'torrent_progress magnetizing');
 				} else {
-					statsElem.appendChild(document.createTextNode(
-						formatBytes(props.sizeWhenDone - props.leftUntilDone) + ' of ' + formatBytes(props.sizeWhenDone) +
-						' (' + percentDone.toFixed(2) + '%) - ' + formatSeconds(props.eta) + ' remaining'
-					));
-					speedsElem.innerHTML = 'DL: ' + formatBytes(props.rateDownload) + '/s UL: ' + formatBytes(props.rateUpload) + '/s';
+					oStats.text(formatBytes(props.sizeWhenDone - props.leftUntilDone) + ' of ' + formatBytes(props.sizeWhenDone)
+								+ ' (' + percentDone.toFixed(2) + '%) - ' + formatSeconds(props.eta) + ' remaining');
+					oSpeeds.text('DL: ' + formatBytes(props.rateDownload) + '/s UL: ' + formatBytes(props.rateUpload) + '/s');
 				}
 			break;
 			case TORRENT_SEEDING:
-				statsElem.appendChild(document.createTextNode(
-					formatBytes(props.sizeWhenDone)
-					+ ' - Seeding, uploaded ' + formatBytes(props.uploadedEver)
-					+ ' (Ratio ' + (props.uploadedEver / props.sizeWhenDone).toFixed(2) + ')'
-				));
-				speedsElem.innerHTML = 'UL: ' + formatBytes(props.rateUpload) + '/s';
-				$(curProgressElem).attr('class', 'seeding');
+				oStats.text(formatBytes(props.sizeWhenDone) + ' - Seeding, uploaded ' + formatBytes(props.uploadedEver)
+					+ ' (Ratio ' + (props.uploadedEver / props.sizeWhenDone).toFixed(2) + ')');
+				oSpeeds.text('UL: ' + formatBytes(props.rateUpload) + '/s');
+				oProgressBar.attr('class', 'seeding');
 			break;
 			case TORRENT_PAUSED:
 				if (props.leftUntilDone) {
-					statsElem.appendChild(document.createTextNode(
-						formatBytes(props.sizeWhenDone - props.leftUntilDone)
-						+ ' of ' + formatBytes(props.sizeWhenDone)
-						+ ' (' + percentDone.toFixed(2) + '%) - Paused'
-					));
+					oStats.text(formatBytes(props.sizeWhenDone - props.leftUntilDone) + ' of ' + formatBytes(props.sizeWhenDone) + ' (' + percentDone.toFixed(2) + '%) - Paused');
 				} else {
 					var done = (props.doneDate > 0) ? props.doneDate : props.addedDate;
-					statsElem.appendChild(document.createTextNode(
-						formatBytes(props.sizeWhenDone)
-						+ ' - Completed on ' + new Date(done * 1000).toLocaleDateString()
-					));
+					oStats.text(formatBytes(props.sizeWhenDone) + ' - Completed on ' + new Date(done * 1000).toLocaleDateString());
 				}
-				speedsElem.innerHTML = '';
-				curProgressElem.className = 'paused';
+				oSpeeds.text('');
+				oProgressBar.attr('class', 'paused');
 			break;
 		}
+	}
+
+	function adjustProgress(percentDone) {
+		if (percentDone === 100) {
+			oProgressBar.attr('class', 'complete').css('width', percentDone + '%');
+		} else if (percentDone > 0) {
+			oProgressBar.attr('class', 'downloading').css('width', percentDone + '%');
+		} else {
+			oProgressBar.hide();
+		}
+	}
+
+	// create the list element and update torrent properties
+	this.createElem = function(props) {
+
+		this.id = props.id || TORRENT_ERROR;
+		this.name = props.name || TORRENT_ERROR;
+		this.status = props.status || TORRENT_ERROR;
+
+		var self = this;
+
+		oName = $('<div>' + props.name + '</div>').attr({
+				'class': 'torrent_name'
+			,	'title': props.name + '\n\nDownloaded to: ' + props.downloadDir
+		}).appendTo(oRoot);
+
+		oStats = $('<div></div>').attr({
+				'class': 'torrent_stats'
+		}).appendTo(oRoot);
+
+		oSpeeds = $('<div></div>').attr({
+				'class': 'torrent_speeds'
+		}).appendTo(oRoot);
+
+		oProgress = $('<div></div>').attr({
+				'class': 'torrent_progress'
+		}).appendTo(oRoot);
+
+		oProgressBar = $('<div></div>').appendTo(oProgress);
+
+		oPauseBtn = $('<div></div>').attr({
+				'class': 'torrent_button pause'
+			,	'title': 'Pause'
+		}).click(function(e) {
+			self.sendRPC('torrent-stop');
+		}).appendTo(oRoot);
+
+		oResumeBtn = $('<div></div>').attr({
+				'class': 'torrent_button resume'
+			,	'title': 'Resume'
+		}).click(function() {
+			self.sendRPC('torrent-start');
+		}).appendTo(oRoot);
+
+		oRemoveBtn = $('<div></div>').attr({
+				'name' : 'torrent_remove'
+			,	'class': 'torrent_button remove'
+			,	'title': 'Double-click to remove torrent\n\nHold down CTRL to also delete data'
+		}).dblclick(function(e) {
+			self.sendRPC('torrent-remove', e.ctrlKey);
+		}).appendTo(oRoot);
+
+		adjustProgress(100 - (props.leftUntilDone / props.sizeWhenDone * 100));
+		adjustButtons(props.status);
+		adjustLabels(props);
 	};
 
 	// update the list element and update torrent properties
 	this.updateElem = function(props) {
-		if (typeof props.status !== 'undefined') this.status = props.status;
+		this.status = props.status || TORRENT_ERROR;
 
-		var percentDone = 100 - (props.leftUntilDone / props.sizeWhenDone * 100),
-			statsElem = this.elem.childNodes[1],
-			speedsElem = statsElem.childNodes[0],
-			progress = $(this.elem.childNodes[2]),
-			progressBar = $(this.elem.childNodes[2].childNodes[0]),
-			pauseElem = this.elem.childNodes[3],
-			resumeElem = this.elem.childNodes[4];
+		oName.attr('title', props.name + '\n\nDownloaded to: ' + props.downloadDir);
+		oProgress.attr('class', 'torrent_progress');
 
-		//name
-		$(this.elem.childNodes[0]).attr('title', props.name + '\n\nDownloaded to: ' + props.downloadDir);
-
-		progress.attr('class', 'torrent_progress');
-
-		if (percentDone === 100) {
-			progressBar.attr('class', 'complete');
-			progressBar.css('width', percentDone + '%');
-		} else if (percentDone > 0) {
-			progressBar.attr('class', 'downloading');
-			progressBar.css('width', percentDone + '%');
-		} else {
-			progressBar.hide();
-		}
-
-		$(pauseElem).toggle(props.status === TORRENT_DOWNLOADING || props.status === TORRENT_SEEDING);
-		$(resumeElem).toggle(props.status === TORRENT_PAUSED);
-
-		switch(props.status) {
-			case TORRENT_WAIT_VERIFY:
-				statsElem.childNodes[1].textContent = formatBytes(props.sizeWhenDone - props.leftUntilDone)
-													+ ' of ' + formatBytes(props.sizeWhenDone)
-													+ ' (' + percentDone.toFixed(2)
-													+ '%) - waiting to verify local data';
-				speedsElem.innerHTML = '';
-			break;
-			case TORRENT_VERIFING:
-				statsElem.childNodes[1].textContent = formatBytes(props.sizeWhenDone - props.leftUntilDone)
-												+ ' of ' + formatBytes(props.sizeWhenDone)
-												+ ' (' + percentDone.toFixed(2)
-												+ '%) - verifying local data ('
-												+ (props.recheckProgress * 100).toFixed() + '%)';
-				speedsElem.innerHTML = "";
-			break;
-			case TORRENT_DOWNLOADING:
-				if (props.metadataPercentComplete < 1) {
-					statsElem.childNodes[1].textContent = 'Magnetized transfer - retrieving metadata ('
-														+ (props.metadataPercentComplete * 100).toFixed() + '%)';
-					speedsElem.innerHTML = '';
-					progress.attr('class', 'torrent_progress magnetizing');
-				} else {
-					statsElem.childNodes[1].textContent = formatBytes(props.sizeWhenDone - props.leftUntilDone)
-											+ ' of ' + formatBytes(props.sizeWhenDone)
-											+ ' (' + percentDone.toFixed(2) + '%) - '
-											+ formatSeconds(props.eta) + ' remaining';
-					speedsElem.innerHTML = 'DL: ' + formatBytes(props.rateDownload) + '/s UL: ' + formatBytes(props.rateUpload) + '/s';
-				}
-			break;
-			case TORRENT_SEEDING:
-				statsElem.childNodes[1].textContent = formatBytes(props.sizeWhenDone)
-												+ ' - Seeding, uploaded ' + formatBytes(props.uploadedEver)
-												+ ' (Ratio ' + (props.uploadedEver / props.sizeWhenDone).toFixed(2) + ')';
-
-				speedsElem.innerHTML = 'UL: ' + formatBytes(props.rateUpload) + '/s';;
-				progressBar.attr('class', 'seeding');
-			break;
-			case TORRENT_PAUSED:
-				if (props.leftUntilDone) {
-					statsElem.childNodes[1].textContent = formatBytes(props.sizeWhenDone - props.leftUntilDone)
-													+ ' of ' + formatBytes(props.sizeWhenDone)
-													+ ' ('+ percentDone.toFixed(2) + '%) - Paused';
-				} else {
-					var done = (props.doneDate > 0) ? props.doneDate : props.addedDate;
-					statsElem.childNodes[1].textContent = formatBytes(props.sizeWhenDone)
-													+ ' - Completed on ' + new Date(done * 1000).toLocaleDateString();
-				}
-
-				progressBar.attr('class', 'paused');
-				speedsElem.innerHTML = '';
-			break;
-		}
+		adjustProgress(100 - (props.leftUntilDone / props.sizeWhenDone * 100));
+		adjustButtons(props.status);
+		adjustLabels(props);
 	};
 
 	// remove the list element for torrent
 	this.removeElem = function() {
-		this.elem.parentNode.removeChild(this.elem);
+		oRoot.remove();
 	};
 }
